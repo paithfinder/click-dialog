222c1a9f1181c187dfa591330649a327a3a9b5e7
谭明乐 <13599304+tan-mingle@user.noreply.gitee.com>
2024-08-16 12:15:24 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,127 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="mask" v-if="showBox">
+    </div>
+    <div class="box">
+      <ul class="car">
+        <li v-for="(item,index) in carList" :key="index" @click="openBox(item.id)">
+          <carItem :name="item.name" :title="item.title"
+          :content="item.content" :money="item.money" :ref="'carItem'+'id'"></carItem>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import carItem from '@/components/car.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components:{
+    carItem
+  },
+  data(){
+    return{
+      carList:[
+        {id:1,name:'住宿一',title:'海滨风情别墅',content:'尽享海风轻拂，风情别墅等你来体验',money:599},
+        {id:2,name:'住宿二',title:'私人海滩小屋',content:'专属海滩，私密小屋，享受独一无二的假期',money:699},
+        {id:3,name:'住宿三',title:'海景露台套房',content:'露台上的海景，套房中的奢华，尽在此处',money:578},
+        {id:4,name:'住宿四',title:'顶级海景豪宅',content:' 奢华至极，顶级海景豪宅，定义高端度假新标准',money:888}
+      ],
+      showBox:false,
+      showId:null
+    }
+  },
+  methods:{
+    openBox(id){
+      console.log(id)
+      console.log(this.$refs)
+      if(!this.showBox){
+        this.showId=id
+        this.$refs.carItemid[id-1].isShow = true
+        this.showBox = true
+        this.$nextTick(()=>{
+          const mask=document.getElementById('mask')
+          mask.addEventListener('click',()=>{
+          this.showBox = false
+          this.$refs.carItemid[this.showId-1].isShow = false
+        })
+        })
+      }
+    }
+  },
+  mounted(){
+    // document.addEventListener('click',()=>{
+    //       if(this.showBox){
+    //         this.$refs.carItemid[this.showId-1].isShow = false
+    //         this.showBox = false
+            
+    //       }else{
+    //         console.log('hah')
+    //         this.showBox = true
+    //       }
+    //     })
+  },
+ 
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  padding:0;
+  margin:0;
+  list-style-type:none;
+
+}
+#app{
+  width:100%;
+  height:100vh;
+  margin:0 auto;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #081c1d;
+}
+
+.box{
+  width:90%;
+  height:90%;
+  margin:0 auto;
+  background-color: #053b31;
+  box-shadow: 0 0 30px #0e9861;
+
+  position:relative;
+  border-radius: 1rem;
+  border:2px solid #0e9861;
+
+
+}
+#mask{
+  width:100%;
+  height:100%;
+  background-color: transparent;
+  position:absolute;
+  z-index:999;
+}
+.car{
+  display:flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  width:50rem;
+  height:30rem;
+  background-color: rgba(18, 77, 60, 0.5);
+  margin:0 auto;
+  position:absolute;
+  top:50%;
+  left:50%;
+  transform: translate(-50%,-50%);
+}
+.car li{
+  width:25rem;
+  height:15rem;
+  display:flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
---
+++ src/components/car.vue
@@ -0,0 +1,154 @@
+<template>
+    <div class="wrap">
+        <img src="../assets/帐篷.png" alt="" style="width:4rem;height:4rem">
+        <div style="color:white">{{ name }}</div>
+        <div class="dialog" v-show="isShow">
+            <div class="text">
+                <div style="font-weight:bold">{{name}}:{{title}}</div>
+                <div>亲情价：{{ money }}元/晚</div>
+                <div class="content">
+                    {{ content }}
+                </div>
+            </div>
+            <div class="move">
+                <img src="../assets/旋转1.png" alt="" class="img1">
+                <img src="../assets/旋转2.png" alt="" class="img2">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'carItem',
+    props: {
+        name: {
+            type: String,
+            required: true,
+            default: () => {
+
+            }
+        },
+        title: {
+            type: String,
+            required: true,
+            default: () => {
+
+            }
+        },
+        content: {
+            type: String,
+            required: true,
+            default: () => {
+
+            }
+        },
+        money: {
+            type: Number,
+            required: true,
+            default: () => {
+
+            }
+        }
+    },
+    data(){
+        return{
+            isShow:false,
+        }
+    },
+    methods:{
+     
+    },
+ 
+}
+</script>
+
+<style>
+.wrap {
+    text-align: center;
+    position:relative;
+}
+.dialog{
+    position:absolute;
+    width:15rem;
+    height:10rem;
+    bottom:4rem;
+    right:8rem;
+    background:linear-gradient(to right,rgb(16,100,84),transparent);
+    border:1px solid #1dcebe;
+    border-right:none;
+    border-radius:5px;
+    display:flex;
+    align-items: center;
+
+}
+.dialog::before{
+    content: "";
+    position: absolute;
+    top: 40%;
+    right: -20px;
+    width: 20px;
+    height: 2px;
+    background: #26f730;
+}
+.dialog::after{
+    content: "";
+    position: absolute;
+    top: 40%;
+    right: -100px;
+    width: 80px;
+    height: 2px;
+    background: repeating-linear-gradient(to right, #26F730, #26F730 2px, transparent 10px, transparent 2px);
+    transform: rotate(60deg);
+    transform-origin: left top;
+}
+.text{
+    font-size:10px;
+    text-align: left;
+    color:white;
+    padding:10px;
+    box-sizing: border-box;
+}
+
+.text div{
+    margin-top:1rem;
+}
+.content{
+    background-color: #23c778;
+    height:4rem;
+    line-height: 1rem;
+    padding:5px;
+    box-sizing: border-box;
+    border-radius: 5px;
+}
+.move{
+    width:45%;
+    height:5rem;
+    position:relative;
+}
+.img2{
+    width:5rem;
+    height:5rem;
+    position:absolute;
+    top:0;
+    left:0;
+    animation:rotate 2s linear infinite;
+}
+.img1{
+    width:2rem;
+    height:2rem;
+    line-height: 3rem;
+    position:absolute;
+    top:30%;
+    left:32.5%;
+    animation:rotate 5s reverse linear infinite;
+}
+@keyframes rotate{
+    0%{
+        transform: rotate(0)
+    }
+    100%{
+        transform:rotate(360deg)
+    }
+}
+</style>
